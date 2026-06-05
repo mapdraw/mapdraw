c9b8aff0981343df5cb415886fa849716edc813b
Aron Sommer <aronsommer@gmail.com>
2026-06-05 21:22:52 +0200

    Use MapDrawTest as created_by on OSM test server

--- js/osm.js
+++ js/osm.js
@@ -398,7 +398,7 @@ async function osmSubmitNode(latlng, tags) {
 
   let changesetId = null;
   try {
-    const changesetXml = `<osm><changeset><tag k="created_by" v="MapDraw"/><tag k="comment" v="Created ${tagComment}"/></changeset></osm>`;
+    const changesetXml = `<osm><changeset><tag k="created_by" v="${OSM_TEST_MODE ? "MapDrawTest" : "MapDraw"}"/><tag k="comment" v="Created ${tagComment}"/></changeset></osm>`;
     const changesetRes = await fetch(`${OSM_API_URL}/changeset/create`, {
       method: "PUT",
       headers,
