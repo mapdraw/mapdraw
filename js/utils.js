a9ee4d6b1d07c6c30ab2826a3d6956ca83c969cf
Aron Sommer <aronsommer@gmail.com>
2026-06-07 14:02:19 +0200

    Escape single quotes in escHtml

--- js/utils.js
+++ js/utils.js
@@ -5,7 +5,8 @@ function escHtml(s) {
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;");
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
 }
 
 /**
