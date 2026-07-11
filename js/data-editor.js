a341978b6313118128c0842fdc8195a801a6d0bc
Aron Sommer <aronsommer@gmail.com>
2026-07-11 13:39:03 +0200

    Fix stale currentRoutePath after Data-editor Apply-to-Map

--- js/data-editor.js
+++ js/data-editor.js
@@ -112,6 +112,7 @@ function applyDataEditor() {
   error.style.display = "none";
 
   deselectCurrentItem();
+  window.app?.clearRouting?.({ skipUiUpdate: true });
   drawnItems.clearLayers();
   editableLayers.clearLayers();
   importedItems.clearLayers();
